508a94fc58bda865693c201254fea2168376d2ef
Vladimir Petrushka <vladimir.petrushka@gmail.com>
2021-12-15 07:44:54 +0300

    Added class JavaPage for w3school

---
+++ src/test/java/pages/w3School/JavaPage.java
@@ -0,0 +1,24 @@
+package pages.w3School;
+
+import driver.BasicDriver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class JavaPage {
+
+    WebDriver driver = BasicDriver.getWebDriver();
+
+    public static final String PAGE_LINK = "https://www.w3schools.com/java/";
+    public static final String TUTORIAL_FIELD = "//span[contains(text(),'Tutorial')]";
+
+
+    public void navigateToPage(){
+        driver.navigate().to(PAGE_LINK);
+    }
+
+    public WebElement getTutorial(){
+        return driver.findElement(By.xpath(TUTORIAL_FIELD));
+    }
+
+}
