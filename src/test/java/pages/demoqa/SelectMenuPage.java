c7de94d065dd762c0ec77bb93b78aa788c0ed0ca
Vladimir Petrushka <vladimir.petrushka@gmail.com>
2021-12-15 08:18:57 +0300

    refactored Classes and elements for SelectMenuPage

--- src/test/java/pages/demoqa/SelectMenuPage.java
+++ src/test/java/pages/demoqa/SelectMenuPage.java
@@ -1,49 +1,36 @@
 package pages.demoqa;
 
+import driver.BasicDriver;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
 
 public class SelectMenuPage {
 
-    WebDriver driver = new ChromeDriver();
+    WebDriver driver = BasicDriver.getWebDriver();
 
-    public void navigateToPage(){
-        driver.navigate().to("https://demoqa.com/select-menu");
+    public static final String SELECT_MENU_PAGE = "https://demoqa.com/select-menu";
+    public static final String WITH_OPT_MENU = "//div[@id = 'withOptGroup']";
+    public static final String MENU_OPTION_NAME = "//div[contains(text(),'%s')]";
+    public static final String SELECT_ONE_MENU = "//div[@id = 'selectOne']";
+    public static final String MULTI_SELECT_DROPDOWN = "//div[contains(text(),'Select...')]";
+    public static final String OLD_STYLE_MENU = "//select[@id = 'oldSelectMenu']";
+    public static final String STANDARD_MULTI_MENU = "//select[@id = 'cars']";
+
+    public void navigateToPage() {
+        driver.navigate().to(SELECT_MENU_PAGE);
     }
 
-    public void getSelectValueMenu(){
-        driver.findElement(By.xpath("//div[@id = 'withOptGroup']"));
+    public void getMenu(String menu) {
+        driver.findElement(By.xpath(menu)).click();
     }
 
-    public void setValueMenu(String name){
-        driver.findElement(By.xpath("//div[contains(text(),'"+name+"')]"));
+    public void setMenu(String name) {
+        driver.findElement(By.xpath(String.format(MENU_OPTION_NAME, name))).click();
     }
 
-    public void getSelectOneMenu(){
-        driver.findElement(By.xpath("//div[@id = 'selectOne']"));
-    }
-
-    public void setOneMenu(String name){
-        driver.findElement(By.xpath("//div[contains(text(),'"+name+"')]"));
-    }
-
-    public void setOldStyleSelectMenu(int value){
-        Select select = new Select(driver.findElement(By.xpath("//select[@id = 'oldSelectMenu']")));
-        select.selectByValue(String.format("%d",value));
-    }
-
-    public void getMultiselectDropDown(){
-        driver.findElement(By.xpath("//div[contains(text(),'Select...')]"));
-    }
-
-    public void setMultiselectDropDown(String color){
-        driver.findElement(By.xpath("//div[contains(text(),'"+color+"')]")).click();
-    }
-
-    public void setStandardMultiSelect(String carManufacture){
-        Select select = new Select(driver.findElement(By.xpath("//select[@id = 'cars']")));
-        select.selectByValue(String.format("%s",carManufacture));
+    public void setAlternativeMenu(String menu, String value) {
+        Select select = new Select(driver.findElement(By.xpath(menu)));
+        select.selectByValue(String.format("%s", value));
     }
 }
