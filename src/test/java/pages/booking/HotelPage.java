8be09277f71f98d78ff9ebc5fe49ac2f668b1967
Vladimir Petrushka <vladimir.petrushka@gmail.com>
2021-12-15 06:59:10 +0300

    added locator and method to get for HotelPage

--- src/test/java/pages/booking/HotelPage.java
+++ src/test/java/pages/booking/HotelPage.java
@@ -3,12 +3,20 @@ package pages.booking;
 import driver.BasicDriver;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 public class HotelPage {
 
     WebDriver driver = BasicDriver.getWebDriver();
 
+    public static final String SCORE_RATING = "//div[@id = 'reviewFloater']//div[contains(@aria-label,' ')]";
+    public static final String REVIEW_FLOATER = "//div[@id = 'reviewFloater']";
+
     public double getHotelRating(){
-        return Double.parseDouble((driver.findElement(By.xpath("//div[@data-testid = 'review-score-right-component']/div[contains(@aria-label,' ')]")).getText()));
+        return Double.parseDouble((driver.findElement(By.xpath(SCORE_RATING)).getText()));
+    }
+
+    public WebElement getReviewFloater(){
+        return driver.findElement(By.xpath(REVIEW_FLOATER));
     }
 }
