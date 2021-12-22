2df70e723b7647dd1ae558f9534e12c0e9c978b2
Vladimir Petrushka <vladimir.petrushka@gmail.com>
2021-12-22 08:55:12 +0300

    added Runner for ChuckNorrisTest

---
+++ src/test/java/runners/chucknorris/Runner.java
@@ -0,0 +1,10 @@
+package runners.chucknorris;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import tests.chucknorris.ChuckNorrisTest;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({ChuckNorrisTest.class})
+public class Runner {
+}
